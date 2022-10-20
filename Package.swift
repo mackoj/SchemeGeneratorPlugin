fac0bec149047fee969d4ab364c85e7d19e70d5c
Jeffrey Macko <mackoj@users.noreply.github.com>
2022-10-21 01:29:02 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.1.2"),
-    .package(url: "https://github.com/mackoj/SchemeGenerator.git", from: "0.1.0"),
+    .package(url: "https://github.com/mackoj/SchemeGenerator.git", from: "0.3.0"),
   ],
   targets: [
     .binaryTarget(
