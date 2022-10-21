14fa78af0516d9e7b0482f94ce861a418ee04913
Jeffrey Macko <github.jm@macko.fr>
2022-10-21 21:25:03 +0200

    Improve message

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
       capability: .command(
         intent: .custom(
           verb: "scheme-generator",
-          description: "This tool generate the schemes."
+          description: "Generate Xcode Schemes based on Package.swift products and conf_scheme_generator.json"
         ),
         permissions: [
           .writeToPackageDirectory(reason: "This plug-in need to update the schemes in the schemesDirectory folder definied in your configuration file."),
